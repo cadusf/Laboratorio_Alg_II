2789da50874a6f3b11a2d97a5aed8714c50aa9dd
CarlosSilva <cadu09carlos@gmail.com>
2025-12-10 00:03:29 -0300

    fim

--- Lab_03/Ex12.c
+++ Lab_03/Ex12.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+void frac(float num, int *inteiro, float *frac);
+
 int main()
 {
-
-
+    float numero, fracio;
+    int inteira;
+    
+    printf("Numero: ");
+    scanf("%f", &numero);
+    
+    frac(numero, &inteira, &fracio);
+    
+    printf("Inteiro: %d\nFracionario: %.2f", inteira, fracio);
+    
     return 0;
+}
+
+void frac(float num, int *inteiro, float *frac)
+{
+    *inteiro = (int)num;
+    *frac = num - *inteiro;
 }
